eef3071dc6e33c78f4ea5703e045b81bb76c2825
omarFarukGit <muhammadabuhafz@gmail.com>
2026-05-22 09:54:41 +0600

    feat: implement login functionality with JWT token generation and error handling

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -20,7 +20,32 @@ const signup = async (req: Request, res: Response) => {
   }
 };
 
-const login = async (req: Request, res: Response) => {};
+const login = async (req: Request, res: Response) => {
+  try {
+    const result = await AuthService.logingUserIntroDB(req.body);
+
+    const { token } = result;
+
+    res.cookie("token", token, {
+      sameSite: "lax",
+      httpOnly: true,
+      secure: false,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Login successful",
+      data: result,
+    });
+  } catch (error: any) {
+    console.log(error);
+
+    res.status(500).json({
+      success: false,
+      message: error.message || "Something went wrong",
+    });
+  }
+};
 
 export const AuthController = {
   signup,
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,7 +1,10 @@
 import type { Request, Response } from "express";
 import { pool } from "../../db";
 import bcrypt from "bcrypt";
-const createUserIntoDB = async (palyload: any) => {
+import jwt from "jsonwebtoken";
+import type { IUser } from "./auth.interface";
+
+const createUserIntoDB = async (palyload: IUser) => {
   const { name, email, password, role } = palyload;
 
   const hashPassword = await bcrypt.hash(password, 10);
@@ -16,6 +19,44 @@ const createUserIntoDB = async (palyload: any) => {
   return result;
 };
 
+const logingUserIntroDB = async (palyload: {
+  email: string;
+  password: string;
+}) => {
+  const { email, password } = palyload;
+  const userData = await pool.query(
+    `
+        SELECT * FROM users WHERE email=$1
+        `,
+    [email],
+  );
+
+  if (userData.rows.length === 0) {
+    throw new Error("Invaild credentials");
+  }
+  //   console.log(userData.rows[0]);
+  const user = userData.rows[0];
+
+  const matchPassword = await bcrypt.compare(password, user.password);
+
+  if (!matchPassword) {
+    throw new Error("Invaild credentials");
+  }
+
+  const jwtPalyload = {
+    id: user.id,
+    name: user.name,
+    role: user.role,
+  };
+
+  const token = jwt.sign(jwtPalyload, "hfsfosfj", { expiresIn: "1d" });
+
+  delete user.password;
+  console.log(token, user);
+  return { token, user };
+};
+
 export const AuthService = {
   createUserIntoDB,
+  logingUserIntroDB,
 };
