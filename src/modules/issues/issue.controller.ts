c3edf968ccd9d5ae29edccb3b11facd9325d21cb
omarFarukGit <muhammadabuhafz@gmail.com>
2026-05-23 06:29:45 +0600

    refactor: improve type imports and error handling in issue controller

--- src/modules/issues/issue.controller.ts
+++ src/modules/issues/issue.controller.ts
@@ -1,4 +1,9 @@
-import { response, type Request, type Response } from "express";
+import {
+  response,
+  type NextFunction,
+  type Request,
+  type Response,
+} from "express";
 import { IssueService } from "./issue.service";
 
 const createIssue = async (req: Request, res: Response) => {
@@ -37,7 +42,11 @@ const getAllIssues = async (req: Request, res: Response) => {
   }
 };
 
-const getSingleIssue = async (req: Request, res: Response) => {
+const getSingleIssue = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   try {
     const result = await IssueService.getSingleIssueFromDB(
       Number(req.params.id),
@@ -48,9 +57,9 @@ const getSingleIssue = async (req: Request, res: Response) => {
       data: result,
     });
   } catch (error: any) {
-    res.status(500).json({
+    res.status(404).json({
       success: false,
-      message: error.message,
+      message: "Issue not found",
     });
   }
 };
@@ -78,13 +87,13 @@ const updateIssue = async (req: Request, res: Response) => {
 
 const deleteIssue = async (req: Request, res: Response) => {
   try {
-    const issueId=Number(req.params.id);
+    const issueId = Number(req.params.id);
     await IssueService.deleteIssueFromDB(issueId);
 
     res.status(200).json({
-      success:true,
-      message:"Issue deleted successfully"
-    })
+      success: true,
+      message: "Issue deleted successfully",
+    });
   } catch (error: any) {
     res.status(404).json({
       success: false,
